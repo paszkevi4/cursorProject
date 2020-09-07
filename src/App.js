436c4496de0a17f0e25de8474e3597141e70229e
paszkevi4 <paszkevi4@gmail.com>
2020-09-07 21:33:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="mainContent"></div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => (
+  <nav>
+    <div>LOGO</div>
+    {/* logo */}
+    {/* navigation */}
+    <p>home</p>
+    <p>charts</p>
+    <p>categories</p>
+  </nav>
+);
+
+export default NavBar;

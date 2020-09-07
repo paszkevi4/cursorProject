fc3b569492a6854c98be9c90553d4ec227f10be0
paszkevi4 <paszkevi4@gmail.com>
2020-09-08 00:16:13 +0300

    Updates

---
+++ precache-manifest.bb9d80cafe79f78f40cdb72e8c1a40a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6a877cd12fd4b81abc91ab05a69db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8a4190270d9aca6a708",
+    "url": "/static/css/main.cf28357a.chunk.css"
+  },
+  {
+    "revision": "785f07aefe3560a4e978",
+    "url": "/static/js/2.1e52ca70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1e52ca70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a4190270d9aca6a708",
+    "url": "/static/js/main.66842161.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);

9a8193b0387fd9a969863015caf14c6d9a0565e7
paszkevi4 <paszkevi4@gmail.com>
2020-09-10 00:37:13 +0300

    crud for charge categories

--- src/redux/chargeCategoriesReducer.ts
+++ src/redux/chargeCategoriesReducer.ts
@@ -1,4 +1,6 @@
 const SET_CATEGORY = 'SET_CATEGORY';
+const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
+const DELETE_CATEGORY = 'DELETE_CATEGORY';
 
 type chargeCategoryType = {
   name: string;
@@ -7,9 +9,20 @@ type chargeCategoryType = {
   icon: number;
 };
 
-type chargeCategoryACType = {
+type setChargeCategoryACType = {
   type: typeof SET_CATEGORY;
   category: chargeCategoryType;
+};
+
+type updateChargeCategoryACType = {
+  type: typeof UPDATE_CATEGORY;
+  category: chargeCategoryType;
+  index: number;
+};
+
+type deleteChargeCategoryACType = {
+  type: typeof DELETE_CATEGORY;
+  index: number;
 };
 
 let initialState: Array<chargeCategoryType> = [
@@ -25,21 +38,38 @@ let initialState: Array<chargeCategoryType> = [
   { name: 'Pets', description: 'For smth else', date: '26/08/20', icon: 14 },
 ];
 
-const chargeCategoriesReducer = (
-  state = initialState,
-  action: chargeCategoryACType,
-): Array<chargeCategoryType> => {
+const chargeCategoriesReducer = (state = initialState, action: any): Array<chargeCategoryType> => {
   switch (action.type) {
     case SET_CATEGORY:
       return [...state, action.category];
+    case UPDATE_CATEGORY:
+      state.splice(action.index, 1, action.category);
+      return [...state];
+    case DELETE_CATEGORY:
+      state.splice(action.index, 1);
+      return [...state];
     default:
       return state;
   }
 };
 
-export const setChargeCategoryAC = (category: chargeCategoryType): chargeCategoryACType => ({
+export const createChargeCategoryAC = (category: chargeCategoryType): setChargeCategoryACType => ({
   type: SET_CATEGORY,
   category,
+});
+
+export const updateChargeCategoryAC = (
+  index: number,
+  category: chargeCategoryType,
+): updateChargeCategoryACType => ({
+  type: UPDATE_CATEGORY,
+  index,
+  category,
+});
+
+export const deleteChargeCategoryAC = (index: number): deleteChargeCategoryACType => ({
+  type: DELETE_CATEGORY,
+  index,
 });
 
 export default chargeCategoriesReducer;

7e1cedb73ac4ee99afea06ce4a4137678f7a65eb
Yuliya <yulia.masechko@gmail.com>
2020-09-09 23:12:06 +0300

    add HomePage with components

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {HashRouter, NavLink, Route} from "react-router-dom";
+import Charges from "./Charges";
+import Incomes from "./Incomes";
 
 // in props you have two arrays of objects:
 
@@ -14,9 +17,43 @@ import React from 'react';
 
 const HomePage = (props) => {
   return (
-    <>
-      <div>HOMEPAGE</div>
-    </>
+      <HashRouter>
+        <div>
+          <div>
+            <div>
+              <h2>HOMEPAGE</h2>
+            </div>
+            <div>
+              <h2>Balance</h2>
+              <h1>$2,652.07</h1>
+            </div>
+          </div>
+          <div>
+            <div>
+              <NavLink to="/homepage/charges">Charges</NavLink>
+              <NavLink to="/homepage/incomes">Incomes</NavLink>
+            </div>
+            <div>
+              <div>
+                {/*<h4>{props.title}</h4>*/}
+                <h4>Charges</h4>
+                {/*<HomeSelect />*/}
+              </div>
+              <button>Add more</button>
+            </div>
+          </div>
+          <div>
+            <Route
+                path="/homepage/charges"
+                render={() => <Charges props={props.charges}/>}
+            />
+            <Route
+                path="/homepage/incomes"
+                render={() => <Incomes props={props.incomes}/>}
+            />
+          </div>
+        </div>
+      </HashRouter>
   );
 };
 

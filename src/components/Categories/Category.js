d39349a907a2c19bc61aca6dd306e99810cb9b31
Liubomyr <louidropson@gmail.com>
2020-09-09 17:00:46 +0300

    finished main layout of the categories page

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -1,18 +1,54 @@
 import React from "react";
 
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+
 const Category = ({ name, description, date, icon }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    console.log(e.currentTarget);
+  };
+
+  const handleClose = (e) => {
+    console.log(e.currentTarget);
+    setAnchorEl(null);
+  };
+
   return (
-    <tr>
-      <td>
+    <TableRow>
+      <TableCell component="th" scope="row">
         {icon}
         {name}
-      </td>
-      <td>{description}</td>
-      <td>{date}</td>
-      <td>
-        <button>:</button>
-      </td>
-    </tr>
+      </TableCell>
+      <TableCell>{description}</TableCell>
+      <TableCell>{date}</TableCell>
+      <TableCell>
+        <Button
+          aria-controls="category-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          size="small"
+        >
+          <MoreVertIcon color="action" />
+        </Button>
+        <Menu
+          id="category-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Edit</MenuItem>
+          <MenuItem onClick={handleClose}>Delete</MenuItem>
+        </Menu>
+      </TableCell>
+    </TableRow>
   );
 };
 
--- src/components/Categories/Incomes.js
+++ src/components/Categories/Incomes.js
@@ -1,34 +1,69 @@
 import React from "react";
+
 import Category from "./Category";
 
-const Charges = ({ props }) => {
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import AddIcon from "@material-ui/icons/Add";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableHead: {
+    backgroundColor: "lightcyan",
+  },
+  addButton: {
+    float: "right",
+  },
+});
+
+const Incomes = ({ props }) => {
+  const classes = useStyles();
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <td>Category</td>
-            <td>Description</td>
-            <td>Date</td>
-            <td>Action</td>
-          </tr>
-        </thead>
-        <tbody>
-          {[
-            ...props.map((el, i) => (
-              <Category
-                name={el.name}
-                description={el.description}
-                date={el.date}
-                icon={el.icon}
-                key={i}
-              />
-            )),
-          ]}
-        </tbody>
-      </table>
+      <Button
+        className={classes.addButton}
+        variant="contained"
+        color="primary"
+        startIcon={<AddIcon />}
+      >
+        Add more
+      </Button>
+      <TableContainer component={Paper}>
+        <Table className={classes.table}>
+          <TableHead className={classes.tableHead}>
+            <TableRow>
+              <TableCell>Category</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {[
+              ...props.incomeCategories.map((el, i) => (
+                <Category
+                  name={el.name}
+                  description={el.description}
+                  date={el.date}
+                  icon={el.icon}
+                  key={i}
+                />
+              )),
+            ]}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
 
-export default Charges;
+export default Incomes;

f07e28fda794acb1064013ee0e680b62cfac51d6
Yuliya <yulia.masechko@gmail.com>
2020-09-10 15:59:15 +0300

    add select and modal

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {HashRouter, NavLink, Route} from "react-router-dom";
 import Charges from "./Charges";
 import Incomes from "./Incomes";
+import './HomePage.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 // in props you have two arrays of objects:
 
@@ -15,32 +19,77 @@ import Incomes from "./Incomes";
 // props.setIncome({newObj})
 // props.setCharge({newObj})
 
+const useStyles = makeStyles({
+  home: {
+    padding: '20px',
+  },
+  sectionToggle: {
+    display: 'flex',
+    marginBottom: '20px',
+  },
+  toggleButton: {
+    textDecoration: 'none',
+    color: 'inherit',
+    letterSpacing: '1px',
+  },
+  toggleButtonActive: {
+    borderBottom: '1px solid cornflowerblue',
+  },
+  headerBlock: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'top',
+    // marginBottom: '100px',
+  },
+  headerBalance: {
+    display: 'flex',
+    flexDirection: 'column',
+    // justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '100px',
+  },
+  balanceTitle: {
+    textAlign: 'right',
+    fontSize: '1.2em',
+    marginBottom: '10px',
+  },
+  balanceAmount: {
+    fontSize: '1.7em',
+    fontWeight: 'bold',
+  },
+});
+
 const HomePage = (props) => {
+  const classes = useStyles();
   return (
       <HashRouter>
-        <div>
-          <div>
+        <div className={classes.home}>
+          <div className={classes.headerBlock}>
             <div>
               <h2>HOMEPAGE</h2>
             </div>
-            <div>
-              <h2>Balance</h2>
-              <h1>$2,652.07</h1>
+            <div className={classes.headerBalance}>
+              <h2 className={classes.balanceTitle}>Balance</h2>
+              <h1 className={classes.balanceAmount}>$2,652.07</h1>
             </div>
           </div>
-          <div>
-            <div>
-              <NavLink to="/homepage/charges">Charges</NavLink>
-              <NavLink to="/homepage/incomes">Incomes</NavLink>
-            </div>
-            <div>
-              <div>
-                {/*<h4>{props.title}</h4>*/}
-                <h4>Charges</h4>
-                {/*<HomeSelect />*/}
-              </div>
-              <button>Add more</button>
-            </div>
+          <div className={classes.sectionToggle}>
+            <Button color="primary">
+              <NavLink
+                  to="/homepage/charges"
+                  className={classes.toggleButton}
+                  activeClassName={classes.toggleButtonActive}>
+                Charges
+              </NavLink>
+            </Button>
+            <Button color="primary">
+              <NavLink
+                  to="/homepage/incomes"
+                  className={classes.toggleButton}
+                  activeClassName={classes.toggleButtonActive}>
+                Incomes
+              </NavLink>
+            </Button>
           </div>
           <div>
             <Route

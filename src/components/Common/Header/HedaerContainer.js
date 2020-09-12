059311e5daea22a0ed61222a1016824615981d03
paszkevi4 <paszkevi4@gmail.com>
2020-09-12 15:06:02 +0300

    header bug fix

--- src/components/Common/Header/HedaerContainer.js
+++ src/components/Common/Header/HedaerContainer.js
@@ -10,7 +10,7 @@ let mapStateToProps = (state) => {
 };
 
 let mapDispatchToProps = (dispatch) => {
-  return null;
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Settings = (props) => {
+const Settings = ({ userName, phoneNumber, showWarning, moneyLimit, percentLimit }) => {
+  const [currentMoneyLimit, setCurrentMoneyLimit] = useState(moneyLimit);
+  const [currentPercentLimit, setCurrentPercentLimit] = useState(percentLimit);
   return (
     <div>
-      <div>{props.userName}</div>
-      <div>{props.phoneNumber}</div>
-      <div>{props.limit}</div>
-      <div>{props.showWarning ? 'warn' : 'not to warn'}</div>
+      <h2>Warn me when remains:</h2>
+      <div>
+        <h3>
+          Remain amount
+          <button
+            onClick={() => {
+              setCurrentMoneyLimit(currentMoneyLimit - 100);
+            }}>
+            less
+          </button>
+          <button
+            onClick={() => {
+              setCurrentMoneyLimit(currentMoneyLimit + 100);
+            }}>
+            more
+          </button>
+        </h3>
+        {currentMoneyLimit}
+      </div>
+      <div>
+        <h3>
+          Remain percent
+          <button
+            onClick={() => {
+              setCurrentPercentLimit(currentPercentLimit - 5);
+            }}>
+            less
+          </button>
+          <button
+            onClick={() => {
+              setCurrentPercentLimit(currentPercentLimit + 5);
+            }}>
+            more
+          </button>
+        </h3>
+        {currentPercentLimit}
+      </div>
+      <button>SAVE</button>
     </div>
   );
 };

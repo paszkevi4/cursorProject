c6f36551a44bd2433c322eab07d8f0a17cca2132
paszkevi4 <paszkevi4@gmail.com>
2020-09-13 23:11:17 +0300

    image upload imp

--- src/components/Settings/components/AvatarUpload.js
+++ src/components/Settings/components/AvatarUpload.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db, storage } from '../../../redux/firebase/firebase';
 
 const AvatarUpload = () => {
+  const [image, setImage] = useState(null);
+  const updateImage = (e) => {
+    e.target.files[0] && setImage(e.target.files[0]);
+  };
+  const updateUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref('images')
+          .child(image.name)
+          .gedDownloadURL()
+          .then((url) => {
+            db.collections('user-info').update({
+              userName: 'Ivan',
+              moneyLimit: 2300,
+              avatar: url,
+            });
+            setImage(null);
+          });
+      },
+    );
+  };
+
   return (
     <div class="image-upload">
       <label for="file-input">
         <img src="https://icon-library.net/images/upload-photo-icon/upload-photo-icon-21.jpg" />
       </label>
-
-      <input id="file-input" type="file" style={{ display: 'none' }} />
+      <input id="file-input" type="file" style={{ display: 'none' }} onChange={updateImage} />
+      <button onClick={updateUpload}>upload</button>
     </div>
   );
 };

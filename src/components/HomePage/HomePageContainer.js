c289f3caeeb718122e54769feacff8ee78e064b8
Yuliya <yulia.masechko@gmail.com>
2020-09-13 18:52:45 +0300

    sorted table

--- src/components/HomePage/HomePageContainer.js
+++ src/components/HomePage/HomePageContainer.js
@@ -6,6 +6,7 @@ import { createIncomeAC, updateIncomeAC, deleteIncomeAC } from '../../redux/inco
 let mapStateToProps = (state) => {
   return {
     icons: state.icons,
+    names: state.name,
     incomeCategories: state.incomeCategories,
     chargeCategories: state.chargeCategories,
     incomes: state.incomes,

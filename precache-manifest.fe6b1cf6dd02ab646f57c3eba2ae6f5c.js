7f8cd49c463739352e8b93170e326b1bd2d72fcd
paszkevi4 <paszkevi4@gmail.com>
2020-09-27 14:29:29 +0300

    Updates

---
+++ precache-manifest.fe6b1cf6dd02ab646f57c3eba2ae6f5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad5cc648826004fa4815a6c4c4998ba",
+    "url": "/cursorProject/index.html"
+  },
+  {
+    "revision": "47bb4c9e78703a16d5bf",
+    "url": "/cursorProject/static/css/2.9754a446.chunk.css"
+  },
+  {
+    "revision": "72a415ec7def2dfb8e16",
+    "url": "/cursorProject/static/css/main.4fa15347.chunk.css"
+  },
+  {
+    "revision": "47bb4c9e78703a16d5bf",
+    "url": "/cursorProject/static/js/2.ea875845.chunk.js"
+  },
+  {
+    "revision": "bbb503df27a5f220748a2e0ce610343a",
+    "url": "/cursorProject/static/js/2.ea875845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a415ec7def2dfb8e16",
+    "url": "/cursorProject/static/js/main.44975abd.chunk.js"
+  },
+  {
+    "revision": "8bcda3a125534f3e4eb6",
+    "url": "/cursorProject/static/js/runtime-main.a53070bf.js"
+  },
+  {
+    "revision": "6a46e223009fccad968fa84c2c2da11c",
+    "url": "/cursorProject/static/media/logo_white.6a46e223.png"
+  }
+]);

b7db306580b85d714f49319189b03f6031e335b2
paszkevi4 <paszkevi4@gmail.com>
2020-09-28 19:22:40 +0300

    settings to props add charges

--- src/components/HomePage/AddCharges.js
+++ src/components/HomePage/AddCharges.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   MenuItem,
   Button,
@@ -9,10 +10,10 @@ import {
   DialogTitle,
   TextField,
   Snackbar,
-} from "@material-ui/core";
-import MuiAlert from "@material-ui/lab/Alert";
+} from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 
-import { ModalStyles } from "../Styles";
+import { ModalStyles } from '../Styles';
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -44,69 +45,68 @@ const AddCharges = ({
   handleClose,
   createCharge,
   currentTable = {
-    currentCategory: "",
-    currentName: "",
-    currentDescription: "",
-    currentDate: "",
-    currentMoney: "",
-    currentIcon: "",
+    currentCategory: '',
+    currentName: '',
+    currentDescription: '',
+    currentDate: '',
+    currentMoney: '',
+    currentIcon: '',
   },
   updateCharge,
   charges,
   chargeCategories,
   total,
   handlePeriodChange,
+  ...props
 }) => {
+  debugger;
   const classes = useStyles();
   const today = new Date();
 
   const values = {
     currentDate: `${today.getFullYear()}-${
-      today.getMonth() + 1 < 10
-        ? `0${today.getMonth() + 1}`
-        : today.getMonth() + 1
+      today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1
     }-${today.getDate()}`,
   };
-
 
   const [name, setName] = useState(`${currentTable.currentName}`);
   const [category, setCategory] = useState(currentTable.currentCategory);
   const [description, setDescription] = useState(currentTable.currentDescription);
   const [date, setDate] = useState(
-    currentTable.currentDate ? currentTable.currentDate : values.currentDate
+    currentTable.currentDate ? currentTable.currentDate : values.currentDate,
   );
   const [money, setMoney] = useState(currentTable.currentMoney);
   const [openAlert, setOpenAlert] = useState(false);
 
   const resetInputs = () => {
-    setName("");
-    setCategory("");
-    setDescription("");
+    setName('');
+    setCategory('');
+    setDescription('');
     setDate(values.currentDate);
-    setMoney("");
+    setMoney('');
   };
 
   const handleCloseDialog = (e) => {
-    if (e.target.innerText === "ADD") {
-      const gap = total - +money  ;
-      if (category >= 0 && money ) {
+    if (e.target.innerText === 'ADD') {
+      const gap = total - +money;
+      if (category >= 0 && money) {
         if (gap <= 200) {
-          const isSure = window.confirm("Are you sure?");
+          const isSure = window.confirm('Are you sure?');
           if (isSure) {
             updateCharge({
               name: chargeCategories[category].name,
               icon: chargeCategories[category].icon,
               category: category,
-	      description: description,
-	      date: new Date(date),
-	      money: +money,
-	    });
-	    handleClose();
-	    resetInputs();
-      handleClickAlert();
-	    handlePeriodChange();
-        }
-        return null;
+              description: description,
+              date: new Date(date),
+              money: +money,
+            });
+            handleClose();
+            resetInputs();
+            handleClickAlert();
+            handlePeriodChange();
+          }
+          return null;
         } else {
           updateCharge({
             name: chargeCategories[category].name,
@@ -123,23 +123,23 @@ const AddCharges = ({
         }
       }
       handlePeriodChange();
-    } else if (e.target.innerText === "CANCEL") {
-          handleClose();
-          resetInputs();
-        }
+    } else if (e.target.innerText === 'CANCEL') {
+      handleClose();
+      resetInputs();
+    }
     // }
   };
 
   const handleChange = (event) => {
-    if (typeof event.target.value === "number") {
+    if (typeof event.target.value === 'number') {
       setCategory(event.target.value);
-    } else if (event.target.id === "selectName") {
+    } else if (event.target.id === 'selectName') {
       setName(event.target.value);
-    } else if (event.target.id === "descriptionInput") {
+    } else if (event.target.id === 'descriptionInput') {
       setDescription(event.target.value);
-    } else if (event.target.id === "datePicker") {
+    } else if (event.target.id === 'datePicker') {
       setDate(event.target.value);
-    } else if (event.target.id === "moneyInput") {
+    } else if (event.target.id === 'moneyInput') {
       setMoney(event.target.value);
     }
   };
@@ -149,7 +149,7 @@ const AddCharges = ({
   };
 
   const handleCloseAlert = (event, reason) => {
-    if (reason === "clickaway") {
+    if (reason === 'clickaway') {
       return;
     }
     setOpenAlert(false);
@@ -161,9 +161,8 @@ const AddCharges = ({
         open={open}
         onClose={handleClose}
         aria-labelledby="add-charge-title"
-        aria-describedby="add-charge-description"
-      >
-        <DialogTitle id="add-charge-title">{"ADD NEW ITEM"}</DialogTitle>
+        aria-describedby="add-charge-description">
+        <DialogTitle id="add-charge-title">{'ADD NEW ITEM'}</DialogTitle>
         <DialogContent className={classes.window}>
           <div>
             <TextField
@@ -174,8 +173,7 @@ const AddCharges = ({
               selected={name}
               value={category}
               onChange={handleChange}
-              fullWidth
-            >
+              fullWidth>
               {[
                 ...chargeCategories.map((el, i) => (
                   <MenuItem key={i} value={i} className={classes.nameSelect}>
@@ -231,11 +229,7 @@ const AddCharges = ({
           </Button>
         </DialogActions>
       </Dialog>
-      <Snackbar
-        open={openAlert}
-        autoHideDuration={4000}
-        onClose={handleCloseAlert}
-      >
+      <Snackbar open={openAlert} autoHideDuration={4000} onClose={handleCloseAlert}>
         <Alert onClose={handleCloseAlert} severity="success">
           Added successfully
         </Alert>
@@ -244,4 +238,12 @@ const AddCharges = ({
   );
 };
 
-export default AddCharges;
+let mapStateToProps = (state) => {
+  return {
+    showWarning: state.settings.showWarning,
+    moneyLimit: state.settings.moneyLimit,
+    percentLimit: state.settings.percentLimit,
+  };
+};
+
+export default connect(mapStateToProps, null)(AddCharges);
